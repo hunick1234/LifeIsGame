7731e41b4bc4c630cd492b44e55d3ff10fb38111
邵軒 胡 <hunick1234@gmail.com>
2023-04-25 00:01:04 +0800

    singin api

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"context"
+	"log"
+
+	"github.com/hunick1234/LIG/controller"
+)
+
 type User struct {
 	Id string `json:"id,omitempty" bson:"_id,omitempty"`
 	*Account
@@ -12,14 +19,35 @@ type Account struct {
 	Password    string `json:"password" bson:"password"` //encod
 }
 
+var connectDB controller.DBConnect
+
+func init() {
+	connetUserDB()
+}
+
+func connetUserDB() {
+	connectDB = *controller.NewDBConnect("user")
+	connectDB.Connect()
+}
+
 func GetUser() *User {
 	//connet db find user
 	return &User{}
 }
 
 func (u *User) InsertOneUser() error {
+	collection := connectDB.DBClient.Collection("user")
 
-	return nil
+	_, err := collection.InsertOne(context.Background(), u)
+
+	defer func() {
+		err = connectDB.Client.Disconnect(context.Background())
+		if err != nil {
+			log.Print("err", err)
+		}
+		log.Print("db disconnect")
+	}()
+	return err
 }
 
 func (u *User) PutUser() {
