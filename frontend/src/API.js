e33a98768eb8f3c18d6bfc1fe0032cda433158d4
邵軒 胡 <hunick1234@gmail.com>
2023-05-03 22:05:56 +0800

    set api

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,5 +1,9 @@
 
 
-export const GET_GAMELIST='http://127.0.0.1:8080/api/v1/gameList';
+export const GET_USERINFO="http://127.0.0.1:8080/api/v1/userInfo";
 
-export const GET_='http://127.0.0.1:8080/api/v1/';
+export const GET_GAMES="http://127.0.0.1:8080/api/v1/games";
+export const GET_GAME_INFO="http://127.0.0.1:8080/api/v1/:gameid";
+
+export const LOGIN="http://127.0.0.1:8080/api/v1/login";
+export const SIGNUP="http://127.0.0.1:8080/api/v1/singin";
